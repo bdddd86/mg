6253f617c45e0ac4477cace920c976182fdc2ee7
bdddd86 <joonbumpark864@gmail.com>
2018-01-09 01:58:53 +0900

    맵 작업중.

---
+++ MiningGrowup/Assets/Script/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum eGameState
+{
+	eWorking_Room,
+	wBuilding_Select,
+}
+
+public class GameController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ MiningGrowup/Assets/Script/PC.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ePCState
+{
+	eNone,		// 구매전
+	eMining,	// 채굴중
+	eBreak,		// 고장
+	eEnoughCoin,// 코인 풀
+}
+
+public class PC : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
